59eb6b5a6ab1ff853f8a4bea482addcd727154b7
wanglianhui <2401267171@qq.com>
2020-03-14 12:46:54 +0800

    第二次提交信息

---
+++ jd/src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+	<h1>花开花落花满天</h1>
+</template>
+<script>
+	
+</script>
+<type>
--- jd/src/views/Home.vue
+++ jd/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
   	<h1>wlj</h1>
+  	<h1>花开花落满天</h1>
     <img alt="Vue logo" src="../assets/logo.png">
     
   </div>
