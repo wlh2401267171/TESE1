aa8eea08570f87bcaf38e62015f89c0bc57c8fc7
wanglianhui <2401267171@qq.com>
2020-03-14 22:14:43 +0800

    搜索栏

--- jd/src/App.vue
+++ jd/src/App.vue
@@ -1,10 +1,45 @@
 <template>
   <div id="app">
-<h1>wlh</h1>
+
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-
+   html{
+   	font-size: calc(100vw/7.5);
+   }
+   body{
+   	 /*background-color: #f6f6f6;
+   	 font-size: 0.28rem;
+   	 color:#666;*/
+	   	margin: 0 auto;
+	    min-width: 6.4rem;
+	    max-width: 10.8rem;
+	    background: #fff;
+	    font-size: 0.28rem;
+	    font-family: -apple-system,Helvetica,sans-serif;
+	    line-height: 1.5;
+	    color: #666;
+	    -webkit-text-size-adjust: 100%!important;
+	    padding-bottom: env(safe-area-inset-bottom);
+   	
+   }
+   .left{
+   	float: left;
+   }
+   .right{
+   	float: right;
+   }
+   .flex{
+   	display:flex;
+   }
+   .clear{
+   	display: block;
+   	clear: none;
+   }
+   .flex-item{
+   	flex:1;
+   }
+   
 </style>
--- jd/src/views/Home.vue
+++ jd/src/views/Home.vue
@@ -1,26 +1,20 @@
 <template>
   <div class="home">
-  	<h1>wlj</h1>
-  	<h1>花开花落满天</h1>
-  	<p>柳丝榆荚子芳菲1</p>
-  	<p>柳丝榆荚子芳菲2</p>
-  	<p>柳丝榆荚子芳菲3</p>
-  	<p>柳丝榆荚子芳菲4</p>
-  	<p>柳丝榆荚子芳菲5</p>
-  	<p>柳丝榆荚子芳菲6</p>
-  	<p>柳丝榆荚子芳菲7</p>
-    <img alt="Vue logo" src="../assets/logo.png">
+  	
+  	
+  	<SearchBar></SearchBar>
     
   </div>
 </template>
 
 <script>
-
+import SearchBar from "../components/SearchBar";
 
 
 export default {
   name: 'Home',
   components: {
+  	SearchBar,
     
   }
 }
