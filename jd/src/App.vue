8441299910c4cc4feba8a97cedd82819e8a448fc
wanglianhui <2401267171@qq.com>
2020-03-14 12:33:52 +0800

    第一次提交信息

---
+++ jd/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+<h1>wlh</h1>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
---
+++ jd/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+  	<h1>wlj</h1>
+    <img alt="Vue logo" src="../assets/logo.png">
+    
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
